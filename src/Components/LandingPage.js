046388f1b3276cccce6678ddf0bf0422bf88736c
mahir22B <mahirbhatt.ga@gmail.com>
2024-10-01 22:56:25 -0400

    vid-changes

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -252,10 +252,13 @@ const LandingPage = ({ handleGoogleLogin }) => {
                 infographics in minutes.
               </Text>
               <Box width="100%" maxW="4xl" borderRadius="xl" overflow="hidden">
-                <video width="100%" height="auto" controls preload="metadata">
-                  <source src="/Vid.mp4" type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
+                <iframe
+                  src="https://drive.google.com/file/d/1scUpIPkJgbTX2ipevm6ZLv4y_E9SCO3z/view?usp=sharing"
+                  width="100%"
+                  height="480"
+                  allow="autoplay"
+                  allowFullScreen
+                ></iframe>
               </Box>
             </VStack>
           </Container>
